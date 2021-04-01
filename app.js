2858bec0e20dc8848a38ffafdad1480cfec78ecc
dalejac <k-da929@hotmail.com>
2021-03-31 22:04:03 -0500

    initial set up

---
+++ app.js
@@ -0,0 +1,41 @@
+function mostrarPeliculas() {
+document.getElementById('Pregunta1').style.display = 'block';
+document.getElementById('respuestas1').style.display = 'block';
+document.getElementById('Pregunta2').style.display = 'block';
+document.getElementById('respuestas2').style.display = 'block';
+document.getElementById('Pregunta3').style.display = 'block';
+document.getElementById('respuestas3').style.display = 'block';
+document.getElementById('Pregunta4').style.display = 'none';
+document.getElementById('respuestas4').style.display = 'none';
+document.getElementById('Pregunta5').style.display = 'none';
+document.getElementById('respuestas5').style.display = 'none';
+document.getElementById('Pregunta6').style.display = 'none';
+document.getElementById('respuestas6').style.display = 'none';
+document.getElementById('pic1').style.display= 'block'
+document.getElementById('pic2').style.display= 'block'
+document.getElementById('pic3').style.display= 'block'
+document.getElementById('pic4').style.display= 'none'
+document.getElementById('pic5').style.display= 'none'
+document.getElementById('pic6').style.display= 'none'
+}
+
+function mostrarArte() {
+document.getElementById('Pregunta4').style.display = 'block';
+document.getElementById('respuestas4').style.display = 'block';
+document.getElementById('Pregunta5').style.display = 'block';
+document.getElementById('respuestas5').style.display = 'block';
+document.getElementById('Pregunta6').style.display = 'block';
+document.getElementById('respuestas6').style.display = 'block';
+document.getElementById('Pregunta1').style.display = 'none';
+document.getElementById('respuestas1').style.display = 'none';
+document.getElementById('Pregunta2').style.display = 'none';
+document.getElementById('respuestas2').style.display = 'none';
+document.getElementById('Pregunta3').style.display = 'none';
+document.getElementById('respuestas3').style.display = 'none';
+document.getElementById('pic4').style.display= 'block'
+document.getElementById('pic5').style.display= 'block'
+document.getElementById('pic6').style.display= 'block'
+document.getElementById('pic1').style.display= 'none'
+document.getElementById('pic2').style.display= 'none'
+document.getElementById('pic3').style.display= 'none'
+}
